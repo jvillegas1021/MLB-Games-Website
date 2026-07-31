141694b187cd24e39c0f081c35d80f94608c4747
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-31 12:41:42 -0400

    Add money_icon function for betting logic

--- frontend/src/utility_functions/icon_functions.js
+++ frontend/src/utility_functions/icon_functions.js
@@ -6,3 +6,10 @@ export function bet_icon(odds_a, odds_b) {
     if (odds_a > odds_b) return "🐶";
     return "🏆"
   }
+
+export function money_icon(team, predicted_winner, place_bet) {
+
+    if (team === predicted_winner && place_bet === true) return "💵";
+
+    return ""
+  }
