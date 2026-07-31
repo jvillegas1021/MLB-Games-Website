078180bcf460e887149e034a72fcfa55d77e5a26
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-31 05:12:52 -0700

    Add files via upload

---
+++ backend/python_pipelines/data_scheduler/run_mlb_games_odds_update.py
@@ -0,0 +1,8 @@
+from backend.python_pipelines.data_pipelines.daily_mlb_games_odds_update import run_daily_mlb_games_odds_update
+
+def mlb_games_odds_update():
+    
+    run_daily_mlb_games_odds_update()
+
+if __name__ == "__main__":
+    mlb_games_odds_update
