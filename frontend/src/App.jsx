d2c32eff85181787f515fb8968d5c53bfa72ff8d
jvillegas1021 <jvillegas1021@yahoo.com>
2026-07-31 17:49:13 +0000

    logo change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -84,7 +84,7 @@ function App() {
     {tab === "matchups" && (
       <>
         <img 
-        src="/website_images/Diamond's Edge Logo.jpg"
+        src="/website_images/Diamonds_Edge_Logo.jpg"
         alt="MLB Logo"
         style={{ width: "200px", marginBottom: "20px" }}
         />
