2315fb588273f5dc30a30216844e37bdf112bf1d
jvillegas1021 <jvillegas1021@yahoo.com>
2026-08-04 14:42:12 +0000

    bet slip bar changes

--- frontend/src/components/DiamondsEdgeResults.jsx
+++ frontend/src/components/DiamondsEdgeResults.jsx
@@ -1,8 +1,38 @@
 import "./DiamondsEdgeResults.css";
-
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts";
 export default function DiamondsEdgeResults({ mlb_games_prediction_results }) {
 
     const prediction_results = mlb_games_prediction_results?.[0];
+
+    const winProbData = [
+    {
+        range: "<50%",
+        accuracy: prediction_results.Win_Prob_Under_50
+    },
+    {
+        range: "50-55%",
+        accuracy: prediction_results.Win_Prob_50_55
+    },
+    {
+        range: "55-60%",
+        accuracy: prediction_results.Win_Prob_55_60
+    },
+    {
+        range: "60-65%",
+        accuracy: prediction_results.Win_Prob_60_65
+    },
+    {
+        range: "65%+",
+        accuracy: prediction_results["Win_Prob_65+"]
+    }
+    ];
 
     if (!prediction_results) {
         return <div>Loading...</div>;
@@ -54,6 +84,22 @@ export default function DiamondsEdgeResults({ mlb_games_prediction_results }) {
                     </div>
                 </div>
 
+                <div style={{ marginTop: "40px" }}>
+                    <h2>Accuracy By Win Probability</h2>
+
+                    <ResponsiveContainer width="100%" height={300}>
+                        <BarChart data={winProbData}>
+                        <XAxis dataKey="range" />
+                        <YAxis />
+                        <Tooltip />
+                        <Bar
+                            dataKey="accuracy"
+                            fill="#16a34a"
+                        />
+                        </BarChart>
+                    </ResponsiveContainer>
+                    </div>
+                    ``
             </div>
 
         </div>
